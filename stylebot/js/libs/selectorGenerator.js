9398605156761f9c74324b4aba7b4b2c4982d73d
Ankit Ahuja <ahuja.ankit@gmail.com>
2011-05-19 19:58:58 +0530

    updated selector generator to use .prop and fixed issues in .attr

--- stylebot/js/libs/selectorGenerator.js
+++ stylebot/js/libs/selectorGenerator.js
@@ -6,73 +6,106 @@
  **/
 
 var SelectorGenerator =  {
-    
+
     generate: function(el, granularityLevel) {
-            if (!el)
-                return null;
-            el = $(el);
-            if (granularityLevel == 'low')
-                return this.inspectLow(el);
-            else if (granularityLevel == 'high')
-                return this.inspectHigh(el, 0);
-            else
-                return this.inspect(el, 0);
+        if (!el)
+            return null;
+
+        $el = $(el);
+
+        if (granularityLevel === 'low')
+            return this.inspectLow($el);
+
+        else if (granularityLevel === 'high')
+            return this.inspectHigh($el, 0);
+
+        else
+            return this.inspect($el, 0);
     },
-    
+
     // inspect an element and return a CSS selector for it. this is the default mode
     inspect: function(el, level) {
-        var elId = el.attr('id');
-        var elClass = $.trim(el.attr("class").replace('stylebot-selected', ''));
-        
-        if (elClass.length != 0)
-        {
-            var classes = elClass.split(" ");
-            var len = classes.length;
-            var response = el[0].tagName.toLowerCase();
-            
-            for (var i = 0; i < len; i++)
-                response += "." + classes[i];
-            
-            return response;
+        var elClass = el.attr("class");
+
+        if (elClass != undefined) {
+            elClass = $.trim(elClass.replace('stylebot-selected', ''));
+
+            if (elClass.length != 0)
+            {
+                var classes = elClass.split(" ");
+                var len = classes.length;
+
+                var selector = el.prop('tagName');
+                selector = selector ? selector.toLowerCase() : "";
+
+                for (var i = 0; i < len; i++)
+                    selector += "." + classes[i];
+
+                return selector;
+            }
         }
-        
-        if (elId.length != 0)
+
+        var elId = el.attr('id');
+        if (elId != undefined)
         {
             return "#" + elId;
         }
-        
+
+        var elTag = el.prop('tagName');
+        elTag = elTag ? elTag.toLowerCase() : "";
+
         // don't go beyond 2 levels up
+        //
         if (level < 2)
-            return this.inspect(el.parent(), level + 1) + " " + el[0].tagName.toLowerCase();
+            return this.inspect(el.parent(), level + 1) + " " + elTag;
         else
-            return el[0].tagName.toLowerCase();
+            return elTag;
     },
-    
+
     inspectHigh: function(el, level) {
         var elId = el.attr('id');
-        if (elId.length != 0)
+
+        if (elId != undefined)
             return "#" + elId;
-        
-        var elClass = $.trim(el.attr("class").replace('stylebot-selected', ''));
-        var elTag = el[0].tagName.toLowerCase();
-        var val;
-        
+
+        var elClass = el.attr("class");
+
+        if (elClass != undefined) {
+            elClass = $.trim(elClass.replace('stylebot-selected', ''));
+        }
+
+        else {
+            elClass = "";
+        }
+
+        var elTag = el.prop('tagName');
+        elTag = elTag ? elTag.toLowerCase : "";
+
+        var selector;
+
         if (level < 1)
         {
-            val = this.inspectHigh(el.parent(), level + 1) + " " + elTag;
-            if (elClass.length != 0)
-                val += "." + elClass;
+            selector = this.inspectHigh(el.parent(), level + 1) + " " + elTag;
+
+            if (elClass.length != 0) {
+                selector += "." + elClass;
+            }
         }
+
         else
         {
-            val = elTag;
-            if (elClass.length != 0)
-                val += "." + elClass;
+            selector = elTag;
+
+            if (elClass.length != 0) {
+                selector += "." + elClass;
+            }
         }
-        return val;
+
+        return selector;
     },
-    
+
     inspectLow: function(el) {
-        return el[0].tagName.toLowerCase();
+        var elTag = el.prop('tagName');
+        return elTag ? elTag.toLowerCase() : "";
     }
 };
