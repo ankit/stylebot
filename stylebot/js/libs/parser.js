09f990ea31d9456ee70bf6d91db6f6e7f2cde3dc
Ankit Ahuja <ahuja.ankit@gmail.com>
2011-05-26 16:03:13 +0530

    Fix for error reporting

--- stylebot/js/libs/parser.js
+++ stylebot/js/libs/parser.js
@@ -806,28 +806,39 @@ CSSParser.prototype = {
   ungetToken: function() {
     this.mLookAhead = this.mToken;
   },
-
+  
   addUnknownAtRule: function(aSheet, aString) {
+    var currentLine = CountLF(this.mScanner.getAlreadyScanned());
     var blocks = [];
     var token = this.getToken(false, false);
-    while (token.isNotNull()) {
+    while (token.isNotNull())
+    {
       aString += token.value;
+      
       if (token.isSymbol(";") && !blocks.length)
         break;
+      
       else if (token.isSymbol("{")
                || token.isSymbol("(")
                || token.isSymbol("[")
-               || token.type == "function") {
+               || token.type == "function")
+      {
         blocks.push(token.isFunction() ? "(" : token.value);
-      } else if (token.isSymbol("}")
+      }
+      
+      else if (token.isSymbol("}")
                  || token.isSymbol(")")
-                 || token.isSymbol("]")) {
+                 || token.isSymbol("]"))
+      {
         if (blocks.length) {
           var ontop = blocks[blocks.length - 1];
+          
           if ((token.isSymbol("}") && ontop == "{")
               || (token.isSymbol(")") && ontop == "(")
-              || (token.isSymbol("]") && ontop == "[")) {
+              || (token.isSymbol("]") && ontop == "["))
+          {
             blocks.pop();
+            
             if (!blocks.length && token.isSymbol("}"))
               break;
           }
@@ -836,7 +847,7 @@ CSSParser.prototype = {
       token = this.getToken(false, false);
     }
 
-    this.addUnknownRule(aSheet, aString);
+    this.addUnknownRule(aSheet, aString, currentLine);
   },
 
   addUnknownRule: function(aSheet, aString, aCurrentLine) {
