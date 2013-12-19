98937aacb1bd95a318724464927bb8752e956056
Ankit Ahuja <ahuja.ankit@gmail.com>
2013-12-19 12:02:42 +0530

    Ran jshint on undo.js

--- stylebot/editor/undo.js
+++ stylebot/editor/undo.js
@@ -7,52 +7,54 @@ stylebot.undo = {
   states: [],
 
   /**
-   * Enables the undo button in editor
+   * Enable the undo button in editor
    */
   enable: function() {
     stylebot.widget.enableUndoButton();
   },
 
   /**
-   * Disables the undo button in editor
+   * Disable the undo button in editor
    */
   disable: function() {
     stylebot.widget.disableUndoButton();
   },
 
   /**
-   * Enables / disables the undo button based on if any states are stored
+   * Enable / disable the undo button based on if any states are stored
    */
   refresh: function() {
-    if (this.isEmpty())
+    if (this.isEmpty()) {
       this.disable();
-    else
+    } else {
       this.enable();
+    }
   },
 
   /**
-   * Checks if the states are empty.
+   * Check if the states are empty.
    * @return {boolean} Returns true if no states exist
    */
   isEmpty: function() {
-    if (this.states.length == 0)
+    if (this.states.length === 0) {
       return true;
-    else
+    } else {
       return false;
+    }
   },
 
   /**
-   * Removes last saved state
+   * Remove last saved state
    */
   pop: function() {
     return this.states.pop();
   },
 
   /**
-   * Adds a new state
+   * Add a new state
    * @param {object} state State to save
    */
   push: function(state) {
     this.states.push(state);
   }
-}
+};
