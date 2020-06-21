89cbd2c4239feb8645434aea697fdd0fc9234f9a
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-06-20 19:40:10 -0700

    update compile script to remove redundant handlebars templates

--- build_scripts/compile_templates.js
+++ build_scripts/compile_templates.js
@@ -1,29 +1,16 @@
-const {
-  execSync
-} = require('child_process');
+const { execSync } = require("child_process");
 
-const templates = [{
-    src: 'dist/browseraction/templates/style.handlebars',
-    dest: 'dist/browseraction/templates/style.js',
-  },
+const templates = [
   {
-    src: 'dist/editor/templates/page.handlebars',
-    dest: 'dist/editor/templates/page.js',
-  },
-  {
-    src: 'dist/options/templates/style.handlebars',
-    dest: 'dist/options/templates/style.js',
-  },
-  {
-    src: 'dist/options/templates/style-modal.handlebars',
-    dest: 'dist/options/templates/style-modal.js',
+    src: "dist/editor/templates/page.handlebars",
+    dest: "dist/editor/templates/page.js",
   },
 ];
 
-templates.forEach(({
-  src,
-  dest
-}) => {
+templates.forEach(({ src, dest }) => {
   console.log(`compiling ${src}...`);
-  const execution = execSync(`./node_modules/handlebars/bin/handlebars ${src} -f ${dest}`);
+
+  const execution = execSync(
+    `./node_modules/handlebars/bin/handlebars ${src} -f ${dest}`
+  );
 });
