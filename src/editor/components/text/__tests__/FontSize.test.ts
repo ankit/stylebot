7f9c6dc6dc5b1cf98c15ea4cf75f6661b7852c7f
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-07-12 17:24:57 -0700

    add simple FontSize test example

---
+++ src/editor/components/text/__tests__/FontSize.test.ts
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils';
+
+import FontSize from '../FontSize.vue';
+import Length from '../../Length.vue';
+
+describe('FontSize.vue', () => {
+  it('should pass "font-size" as the "property" prop to Length', () => {
+    const fontSizeWrapper = shallowMount(FontSize, {
+      stubs: ['b-row'],
+    });
+
+    const lengthWrapper = fontSizeWrapper.findComponent(Length);
+    expect(lengthWrapper.props().property).toBe('font-size');
+  });
+});
