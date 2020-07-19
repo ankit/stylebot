8f58c2269e4ada3257057a69d71a09b59ee56d05
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-07-18 17:46:01 -0700

    remove saveOption from background window obj

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,20 +1,10 @@
 import 'crx-hotreload';
+
 import Cache from './cache';
-import Options from './options';
 import Listeners from './listeners';
 import ContextMenu from './contextmenu';
-
-declare global {
-  interface Window {
-    // todo
-    saveOption: any;
-  }
-}
 
 Cache.init((styles, options) => {
   ContextMenu.init();
   Listeners.init(styles, options);
 });
-
-// todo: Find alternative approach to make methods accessible to Options page
-window.saveOption = Options.saveOption;
