94a238c82a5f156532c9bdff4ea62864b004d926
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-07-24 04:15:09 -0700

    Updates

---
+++ component---src-pages-page-2-js-ef8c063115e3c48e159a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-ef8c063115e3c48e159a.js.map
