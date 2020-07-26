257e09a4eb8de0a06445127cbde3224b03ae9a83
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-07-25 22:56:24 -0700

    add gatsby-plugin-google-analytics to docs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -27,8 +27,11 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: process.env.DOCS_GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      },
+    },
   ],
 }
