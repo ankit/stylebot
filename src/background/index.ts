e845f8b8429856362f2256d9842869a7bb7e77d9
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-08-02 17:36:47 -0700

    remove notification manager

--- src/background/index.ts
+++ src/background/index.ts
@@ -4,7 +4,6 @@ import Cache from './cache';
 import Listeners from './listeners';
 import ContextMenu from './contextmenu';
 import MigrateOldFormat from './migrate';
-import NotificationManager from './notification-manager';
 import VersionBasedUpdate from './version-based-update';
 
 (async () => {
@@ -18,7 +17,6 @@ import VersionBasedUpdate from './version-based-update';
   }
 
   Listeners.init(styles, options);
-  NotificationManager.init();
 
   chrome.browserAction.setBadgeBackgroundColor({
     color: '#555',
