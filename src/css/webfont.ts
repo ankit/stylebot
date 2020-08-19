4c82f6ac8079471ba12afab1d57a506a728281d8
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-08-18 17:01:39 -0700

    fix typo in css/webfont

--- src/css/webfont.ts
+++ src/css/webfont.ts
@@ -5,8 +5,7 @@ const getGoogleFontUrlAndParams = (
 ): { url: string; params: string } => {
   const arg = value.replace(' ', '+');
   const url = `https://fonts.googleapis.com/css2?family=${arg}:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap`;
-
-  https: const params = `url(${url})`;
+  const params = `url(${url})`;
 
   return { url, params };
 };
