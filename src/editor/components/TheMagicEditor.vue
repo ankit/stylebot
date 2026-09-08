de7ee3f12ca5e94bcdddd407a9685136c5228783
Ankit Ahuja <ahuja.ankit@gmail.com>
2026-09-07 20:07:51 -0400

    Clean up Magic mode panel spacing and copy (#864)

--- src/editor/components/TheMagicEditor.vue
+++ src/editor/components/TheMagicEditor.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <p class="lead text-muted px-3 pt-3">
-      {{ t('magic_mode_description') }}
-    </p>
-
-    <div class="mt-4 px-3 magic-section">
+    <div class="mt-3 px-3 magic-section">
       <the-readability />
     </div>
 
-    <div class="mt-5 px-3 pt-4 magic-section">
+    <div class="mt-4 px-3 magic-section">
       <h1 class="magic-title">{{ t('grayscale') }}</h1>
       <the-grayscale />
     </div>
@@ -38,11 +34,7 @@ export default Vue.extend({
 }
 
 .magic-section {
-  // padding: 15px 10px 0 10px;
-  border-top: 1px solid #ddd;
-
   &:first-of-type {
-    border: none;
     margin-top: 0;
   }
 }
